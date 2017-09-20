4f75a5cade49013b4b763355aa113647cdebd2a2
rajdeep66 <rajdeep.jgd@gmail.com>
2017-09-20 16:33:29 +0530

    final

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { NavComponent } from './nav/nav.component';
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
@@ -6,7 +7,9 @@ describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        NavComponent,
+
       ],
     }).compileComponents();
   }));
--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -1,27 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http , Response} from '@angular/http';
 import { RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
 @Injectable()
 export class SearchService {
     constructor(private http: Http) {};
     restaurants = [];
     cuisine:any;
     getResturant(cuisine:HTMLInputElement, city:HTMLInputElement){
-        
-      
         let headers = new Headers({ 'user-key' : '2070452acff812f8390d2927eb2ceed0' });
         let requestOpt = new RequestOptions({ headers: headers });
-       
-        var apiLink = 'https://developers.zomato.com/api/v2.1/search?entity_id='+(city.value)+'&entity_type=city&cuisines=' + (cuisine.value);
+        var apiLink = 'https://developers.zomato.com/api/v2.1/search?entity_id='
+        +(city.value)+'&entity_type=city&cuisines=' + (cuisine.value);
         // console.log(cuisine.value);
-        
         this.http.get(apiLink, requestOpt)
         .subscribe((res: Response) => {
           this.restaurants = res.json().restaurants;
-          
         });
   }
-
+  
   getCuisine(city:HTMLInputElement):any{
     let headers = new Headers({ 'user-key' : '2070452acff812f8390d2927eb2ceed0' });
     let requestOpt = new RequestOptions({ headers: headers });
